bf3f9b0578749fa0aa5bb0487ef6b028aa44246f
Harmeet Singh <harmeet@golfdiscount.com>
2021-07-15 12:22:16 -0700

    Initial commit

---
+++ wsi-order-trigger/config.py
@@ -0,0 +1,8 @@
+config = {
+    "mysql": {
+        "user": "harmeet_pgd@wsi-orders",
+        "pass": "%a5xe2*S4xxd^MDmyqFFd!qrwSGKBPN&QCPiXtDV&&=xE7=SyumA%>:zLGEuo].E",
+        "host": "wsi-orders.mysql.database.azure.com",
+        "database": "wsi"
+    }
+}
