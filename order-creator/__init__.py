0890f3b25903d33e033dd0fc945702e7a2c7d11c
Harmeet Singh <harmeet@golfdiscount.com>
2021-07-20 15:52:22 -0700

    Initial commit

--- order-creator/__init__.py
+++ order-creator/__init__.py
@@ -1,24 +1,59 @@
 import logging
-
 import azure.functions as func
+from .pickticket.pickticket import Ticket
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
+        header = createHeader(req)
+        detail = createDetail(req)
+    except:
+        return func.HttpResponse("There was an error creating the ticket", status_code=500)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    ticket = Ticket()
+    ticket.create_ticket(header, detail)
+
+    with open(f"{header['order_num']}.csv", 'w+') as f:
+        f.writelines(str(ticket))
+        bytes(f)
+        return func.HttpResponse(f.read(), mimetype='text/plain', status_code=200)
+
+def createHeader(req: func.HttpResponse) -> dict:
+    header = {}
+
+    header["pick_ticket_num"] = f"C{req.form['order_num']}"
+    header["order_num"] = req.form["order_num"]
+    header["order_date"] = req.form["order_date"]
+
+    # Customer information
+    header["sold_to_name"] = req.form["sold_to_name"]
+    header["sold_to_address"] = req.form["sold_to_address"]
+    header["sold_to_city"] = req.form["sold_to_city"]
+    header["sold_to_state"] = req.form["sold_to_state"]
+    header["sold_to_country"] = req.form["sold_to_country"]
+    header["sold_to_zip"] = req.form["sold_to_zip"]
+
+    # Recipient information
+    header["ship_to_name"] = req.form["ship_to_name"]
+    header["ship_to_address"] = req.form["ship_to_address"]
+    header["ship_to_city"] = req.form["ship_to_city"]
+    header["ship_to_state"] = req.form["ship_to_state"]
+    header["ship_to_country"] = req.form["ship_to_country"]
+    header["ship_to_zip"] = req.form["ship_to_zip"]
+    header["ship_method"] = req.form["ship_method"]
+
+    return header
+
+def createDetail(req: func.HttpResponse) -> dict:
+    detail = {}
+
+    detail["pick_ticket_num"] = f"C{req.form['order_num']}"
+    detail["line_num"] = 1
+    detail["sku"] = req.form["sku"]
+    detail["quantity"] = req.form["quantity"]
+    detail["units_to_ship"] = detail["quantity"]
+    detail["unit_price"] = req.form["price"]
+
+    return detail
