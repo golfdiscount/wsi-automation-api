e3674b5a997642cd352bccbe1d93228899145666
Harmeet Singh <harmeet@golfdiscount.com>
2021-08-02 11:18:59 -0700

    Added a way to upload orders in JSON format

--- wsi-order-trigger/__init__.py
+++ wsi-order-trigger/__init__.py
@@ -8,82 +8,99 @@ from .pickticket.pickticket import Ticket
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
     try:
-        file = req.files.get('file')
-
-        if file is None:
-            return func.HttpResponse("Please provide a file to upload", status_code=400)
-
         cnx = mysql.connector.connect(user=config["mysql"]["user"],
-                                    password=config["mysql"]["pass"],
-                                    host=config["mysql"]["host"],
-                                    database=config["mysql"]["database"])
+                                password=config["mysql"]["pass"],
+                                host=config["mysql"]["host"],
+                                database=config["mysql"]["database"])
         cursor = cnx.cursor()
 
         logging.info("ShipStation API requester initializing...")
 
         requester = Requester("https://ssapi.shipstation.com", "ssapi.shipstation.com")
         requester.encode_base64("3b72e28b4eb547ab976cc0ac8b1a0662", "fe2bbc64d7de426c8c298b4107dac60a")
-
-        pick_ticket = Ticket()
-        pick_ticket.read_csv(file)
-
-        orders = pick_ticket.get_orders()
-
-        upload_to_api(cursor, orders, requester)
-
-        logging.info("Committing data to database...")
-        cnx.commit()
-        logging.info("Data commited")
-        logging.info("Closing connection to database...")
-        cnx.close()
-
     except Exception as e:
-        return func.HttpResponse(f"There was an error uploading the file\n{str(e)}", status_code=500)
+        return func.HttpResponse(f"There was an error connecting to either the database or ShipStation\n{str(e)}", status_code=500)
 
-    return func.HttpResponse(f"The file {file.filename} has uploaded successfully")
+    
+    file = req.files.get('file')
+
+    if file is not None:
+        upload_file(cursor, file, requester)
+    else:
+        json = req.get_json()
+        try:
+            upload_json(cursor, json, requester)
+        except KeyError as e:
+            return func.HttpResponse(f"The JSON request is malformed\n{e}", status_code=400)
+
+    logging.info("Committing data to database...")
+    cnx.commit()
+    logging.info("Data commited")
+    logging.info("Closing connection to database...")
+    cnx.close()
+
+    return func.HttpResponse(f"The order(s) have uploaded successfully.")
+
+def upload_file(cursor: mysql.connector.connection, file, requester: Requester) -> None:
+    pick_ticket = Ticket()
+    pick_ticket.read_csv(file)
+
+    orders = pick_ticket.get_orders()
+
+    _upload_to_api(cursor, orders, requester)
 
 
-def add_sku_names(orders, requester):
+def upload_json(cursor: mysql.connector.connection, json: dict, requester: Requester) -> None:
     """
-    Adds SKU names to all pick ticket details
-
-    @type orders: dict
-    @param orders: Set of headers and details
+    Uploads an order in JSON format to the WSI DB
+    
+    @type cursor: mysql.connector.connection
+    @param cursor: Connection to the WSI database
+    @type json: dict
+    @param json: JSON data containing order information
     @type requester: Requester object
-    @param requester: Requester object used to make requests to ShipStation API
-    @rtype: dict
-    @return: A set of headers and details with sku names added
+    @param requester: Connection to ShipStation API
     """
-    for ticket in orders:
-        details = orders[ticket]["details"]
-        for detail in details:
-            sku = details[detail].get_sku()
-            details[detail].set_sku_name(get_sku_name(requester.get("/products", {"sku": sku}), sku))
+    logging.info(f"Uploading order {json['order_num']}")
 
-    return orders
+    # Add customer information
+    sold_to_id = wsi.add_cus(cursor, json)
+
+    # Add recipient information
+    ship_to_id = wsi.add_rec(cursor, json)
+
+    # Add order information
+    wsi.add_order(cursor, {
+        "pick_ticket_num": json["pick_ticket_num"],
+        "order_num": json["order_num"],
+        "sold_to": sold_to_id,
+        "ship_to": ship_to_id,
+        "ship_method": json["ship_method"],
+        "order_date": json["order_date"]
+    })
+
+    sku = json["sku"]
+    json["sku_name"] = _get_sku_name(requester.get("/products", {"sku": sku}), sku)
+
+    # Add product
+    wsi.add_product(cursor, {
+        "sku": json["sku"],
+        "sku_name": json["sku_name"],
+        "unit_price": json["unit_price"]
+    })
+
+    # Add line item
+    wsi.add_lt(cursor, {
+        "pick_ticket_num": json["pick_ticket_num"],
+        "line_num": json["line_num"],
+        "units_to_ship": json["units_to_ship"],
+        "sku": json["sku"],
+        "quantity": json["quantity"]
+    })
 
 
-def get_sku_name(response, target):
-    """
-    Gets a target sku from a set of SKUs received from ShipStation
-
-    @type response: dict
-    @param response: List of products from ShipStation
-    @type target: String
-    @param target: Target string to be searched for
-    @rtype: String or None
-    @return: The matching SKU or None if it cannot be found
-    """
-    for product in response["products"]:
-        if product["sku"] == str(target):
-            return product["name"]
-    return None
-
-
-def upload_to_api(cursor, orders, requester):
+def _upload_to_api(cursor, orders, requester):
     """
     Breaks down a header and a detail for each order and inserts them into the database
 
@@ -95,14 +112,13 @@ def upload_to_api(cursor, orders, requester):
     for ticket in orders:
         header = orders[ticket]["header"]
         logging.info(f"Uploading order {header.get_pick_num()}")
-        logging.info("")
-        upload_header(cursor, header)
+        _upload_header(cursor, header)
 
         for detail in orders[ticket]["details"]:
-            upload_detail(cursor, orders[ticket]["details"][detail], requester)
+            _upload_detail(cursor, orders[ticket]["details"][detail], requester)
 
 
-def upload_header(cursor, header):
+def _upload_header(cursor: mysql.connector.connection, header) -> None:
     """
     Uploads information from a header to the WSI API
 
@@ -129,7 +145,7 @@ def upload_header(cursor, header):
     })
 
 
-def upload_detail(cursor, detail, requester):
+def _upload_detail(cursor: mysql.connector.connection, detail, requester: Requester):
     """
     Uploads information from a detail record to the WSI API
 
@@ -139,7 +155,7 @@ def upload_detail(cursor, detail, requester):
     @param detail: Detail record to be uploaded
     """
     sku = detail.get_sku()
-    detail.set_sku_name(get_sku_name(requester.get("/products", {"sku": sku}), sku))
+    detail.set_sku_name(_get_sku_name(requester.get("/products", {"sku": sku}), sku))
     detail = detail.get_pick_details()
     # Add product
     wsi.add_product(cursor, {
@@ -156,3 +172,20 @@ def upload_detail(cursor, detail, requester):
         "sku": detail["sku"],
         "quantity": detail["quantity"]
     })
+
+
+def _get_sku_name(response, target) -> str:
+    """
+    Gets a target sku from a set of SKUs received from ShipStation
+
+    @type response: dict
+    @param response: List of products from ShipStation
+    @type target: String
+    @param target: Target string to be searched for
+    @rtype: String or None
+    @return: The matching SKU or None if it cannot be found
+    """
+    for product in response["products"]:
+        if product["sku"] == str(target):
+            return product["name"]
+    return None
