d772a326abf20185fdd2ec91c947e4ef1b58d13a
Harmeet Singh <harmeet@golfdiscount.com>
2021-10-03 16:30:39 -0700

    Removed configuration file

--- importOrder/config.py
+++
@@ -1,8 +0,0 @@
-config = {
-    "mysql": {
-        "user": "harmeet_pgd@wsi-orders",
-        "pass": "%a5xe2*S4xxd^MDmyqFFd!qrwSGKBPN&QCPiXtDV&&=xE7=SyumA%>:zLGEuo].E",
-        "host": "wsi-orders.mysql.database.azure.com",
-        "database": "wsi"
-    }
-}
