ec0cd3facd3036b058f9ab8cf0569b214b762420
Harmeet Singh <harmeet@golfdiscount.com>
2022-01-31 23:48:18 -0800

    Updated file extension

--- exportOrder/__init__.py
+++ exportOrder/__init__.py
@@ -44,7 +44,7 @@ def upload_sftp(order: tempfile._TemporaryFileWrapper):
         with SFTPClient.from_transport(client.get_transport()) as sftp:
             now = datetime.datetime.now()
             date_string = now.strftime(f"%m_%d_%Y_%H_%M_%S")
-            file_name = f'PT_WSI_{date_string}'
+            file_name = f'PT_WSI_{date_string}.csv'
 
             logging.info(f'Uploading {file_name} to {os.environ["target"]} directory')
             remote_path = os.path.join(os.environ["target"], file_name)
