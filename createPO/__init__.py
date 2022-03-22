2747567bb1b1da47d832bf57d1d3b73a2ef8911a
Harmeet Singh <harmeet@golfdiscount.com>
2022-03-22 16:29:23 -0700

    Fixed bug with pulling CSVs from server

--- createPO/__init__.py
+++ createPO/__init__.py
@@ -2,36 +2,47 @@ import azure.functions as func
 import logging
 import os
 import pandas as pd
+import requests
 
 from azure.storage.blob import BlobClient
+from io import BytesIO
 
 def main(timer: func.TimerRequest) -> None:
-    master: pd.DataFrame = pd.read_csv(os.environ['PO_MASTER_URL'])
-    daily: pd.DataFrame = pd.read_csv(os.environ['PO_DAILY_URL'])
+    master: requests.Response = requests.get(os.environ['PO_MASTER_URL'])
+    daily: requests.Response = requests.get(os.environ['PO_DAILY_URL'])
 
-    for row in daily.itertuples():
-        temp: pd.DataFrame = master[master['PO Header'] == row[1]]
-        created_date = temp['Date Expected'].tolist()
+    master.raise_for_status()
+    daily.raise_for_status()
 
-        if len(created_date) > 0:
-            created_date = created_date[0]
-            expected_date = temp['Date Expected'].iloc[0]
-            temp = temp.iloc[:, :27]
+    master: pd.DataFrame = pd.read_csv(BytesIO(master.content))
+    daily: pd.DataFrame = pd.read_csv(BytesIO(daily.content))
 
-            header: pd.DataFrame = pd.DataFrame([['ROH', 'I', 'P', row[1], created_date,
-                None, None, expected_date, None, None, 'PGD', 'HN', None,
-                None, None, None, None, None, None, None, None, None]])
-            logging.info(f'Generating {len(temp)} SKU(s) for PO {row[1]}')
+    if daily.size == 0:
+        logging.warning('There are no POs in the daily file')
+    else:
+        for row in daily.itertuples():
+            temp: pd.DataFrame = master[master['PO Header'] == row[1]]
+            created_date = temp['Date Expected'].tolist()
 
-            po_csv = header.to_csv(header=False, index=False)
-            po_csv += temp.to_csv(header=False, index=False)
-            export_po(po_csv, f'RO_{row[1]}.csv')
-        else:
-            logging.warn(f'No SKUs found for PO {row[1]}')
+            if len(created_date) > 0:
+                created_date = created_date[0]
+                expected_date = temp['Date Expected'].iloc[0]
+                temp = temp.iloc[:, :27]
+
+                header: pd.DataFrame = pd.DataFrame([['ROH', 'I', 'P', row[1], created_date,
+                    None, None, expected_date, None, None, 'PGD', 'HN', None,
+                    None, None, None, None, None, None, None, None, None]])
+                logging.info(f'Generating {len(temp)} SKU(s) for PO {row[1]}')
+
+                po_csv = header.to_csv(header=False, index=False)
+                po_csv += temp.to_csv(header=False, index=False)
+                export_po(po_csv, f'RO_{row[1]}.csv')
+            else:
+                logging.warn(f'No SKUs found for PO {row[1]}')
 
 def export_po(po: str, po_name: str) -> None:
     blob = BlobClient.from_connection_string(os.environ['AzureWebJobsStorage'],
         container_name='wsi-pos',
         blob_name=po_name,)
-    
+
     blob.upload_blob(po, overwrite=True)
