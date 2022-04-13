7377b13a0a6d1a522d61de473184aa2b0236c87d
Harmeet Singh <harmeet@golfdiscount.com>
2022-04-13 15:05:44 -0700

    Initial commit

---
+++ createItemMasterList/__init__.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+import azure.functions as func
+import mysql.connector as sql
+import os
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    
+    db: sql.MySQLConnection = sql.connect(
+        user = 'dbread',
+        password = os.environ['EAGLE_OASS'],
+        host = os.environ['EAGLE_HOST'],
+        database = os.environ['EAGLE_DB']
+    )
+
+    cursor = db.cursor()
+
+    qry = 'SELECT * FROM dw_store;'
+
+    cursor.execute(qry)
+
+    for row in cursor:
+        for column in row:
+            print(column)
+        print('-' * 50)
+
+    return func.HttpResponse('hello')
