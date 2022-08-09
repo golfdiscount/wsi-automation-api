5ba2e59601b2e55efd7a0cfe1751bb478b35b8f5
Harmeet Singh <harmeet@golfdiscount.com>
2022-08-09 00:49:30 -0700

    Initial commit

---
+++ Models/Header.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace wsi_triggers.Models
+{
+    public class Header
+    {
+        public string PickticketNumber { get; set; }
+        public string OrderNumber { get; set; }
+        public char Action { get; set; }
+        public int Store { get; set; }
+        public int Customer { get; set; }
+        public int Recipient { get; set; }
+        public int ShippingMethod { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int Channel { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
