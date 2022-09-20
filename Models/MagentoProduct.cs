0f654feb98d32e957daebcceafd4f4e6f28a6405
Harmeet Singh <harmeet@golfdiscount.com>
2022-09-19 21:09:05 -0700

    Initial commit

---
+++ Models/MagentoProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace wsi_triggers.Models
+{
+    public class MagentoProduct
+    {
+        public string Name { get; set; }
+        public string Sku { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Upc { get; set; }
+    }
+}
