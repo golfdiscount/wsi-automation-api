f544cf043ecda3e989beaadb914545f82373d775
Harmeet Singh <harmeet@golfdiscount.com>
2022-09-26 18:52:03 -0700

    Added DI services for blob containers and queues

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
 using Renci.SshNet;
 using System;
@@ -45,6 +46,7 @@ namespace wsi_triggers
             builder.Services.AddSingleton(jsonOptions);
             builder.Services.AddSingleton(connectionBuilder);
             builder.Services.AddSingleton(sftp);
+
             builder.Services.AddHttpClient("magento", config =>
             {
                 KeyVaultSecret magentoUri = secretClient.GetSecret("magento-uri");
@@ -52,6 +54,21 @@ namespace wsi_triggers
 
                 config.BaseAddress = new(magentoUri.Value);
                 config.DefaultRequestHeaders.Add("x-functions-key", magentoKey.Value);
+            });
+            builder.Services.AddHttpClient("dufferscorner", config =>
+            {
+                KeyVaultSecret duffersUri = secretClient.GetSecret("dufferscorner-uri");
+
+                config.BaseAddress = new(duffersUri.Value);
+            });
+
+            builder.Services.AddAzureClients(clientBuilder =>
+            {
+                clientBuilder.AddBlobServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+                clientBuilder.AddQueueServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage")).ConfigureOptions(config =>
+                {
+                    config.MessageEncoding = Azure.Storage.Queues.QueueMessageEncoding.Base64;
+                });
             });
         }
     }
