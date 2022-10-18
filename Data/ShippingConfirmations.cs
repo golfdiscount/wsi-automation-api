38ea3581c29cf37316074362a64d290a3b415b27
Harmeet Singh <harmeet@golfdiscount.com>
2022-10-18 11:46:34 -0700

    Initial commit

---
+++ Data/ShippingConfirmations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wsi_triggers.Data
+{
+    internal class ShippingConfirmations
+    {
+    }
+}
