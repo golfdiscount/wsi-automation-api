5a4c4956aa1d443946600b6c690ddba7281e82f7
Harmeet Singh <harmeet@golfdiscount.com>
2022-10-18 11:46:11 -0700

    Initial commit

---
+++ Models/ShippingConfirmation/GetShippingMethodModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace wsi_triggers.Models.ShippingConfirmation
+{
+    public class GetShippingMethodModel
+    {
+        public string PickticketNumber { get; set; }
+        
+        public int LineNumber { get; set; }
+
+        public DateTime ShipDate { get; set; }
+        
+        public string TrackingNumber { get; set; }
+
+        public string ShippingMethod { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
