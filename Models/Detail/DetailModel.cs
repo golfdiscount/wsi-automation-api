528bd13eaf34f63f31051443887c866957f633b0
Harmeet Singh <harmeet@golfdiscount.com>
2022-10-19 13:34:10 -0700

    Consolidated models into one

--- Models/Detail/DetailModel.cs
+++ Models/Detail/DetailModel.cs
@@ -10,5 +10,7 @@ namespace WsiApi.Models.Detail
         public string Sku { get; set; }
         public int Units { get; set; }
         public int UnitsToShip { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
     }
 }
