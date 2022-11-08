d53f05bd91ac96fd0819bdcbfb31bb04dd6282df
Harmeet Singh <harmeet@golfdiscount.com>
2022-11-08 12:32:16 -0800

    Delets blob after successful SFTP

--- Blob Triggers/SftpBlob.cs
+++ Blob Triggers/SftpBlob.cs
@@ -1,21 +1,26 @@
+using Azure.Storage.Blobs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Renci.SshNet;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace WsiApi.Blob_Triggers
 {
     public class SftpBlob
     {
+        private readonly BlobServiceClient blobServiceClient;
         private readonly SftpClient sftp;
-        public SftpBlob(SftpClient sftp)
+
+        public SftpBlob(SftpClient sftp, BlobServiceClient blobServiceClient)
         {
+            this.blobServiceClient = blobServiceClient;
             this.sftp = sftp;
         }
 
         [FunctionName("SftpBlob")]
-        public void Run([BlobTrigger("sftp/{name}", Connection = "AzureWebJobsStorage")]Stream blob, string name, ILogger log)
+        public async Task Run([BlobTrigger("sftp/{name}", Connection = "AzureWebJobsStorage")]Stream blob, string name, ILogger log)
         {
             log.LogInformation($"Initializing SFTP for {name}");
             sftp.Connect();
@@ -23,9 +28,13 @@ namespace WsiApi.Blob_Triggers
             try
             {
                 sftp.UploadFile(blob, $"Inbound/{name}");
+                BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient("sftp");
+                log.LogInformation($"Deleting {name} from sftp container");
+                await blobContainerClient.DeleteBlobAsync(name);
+
             } catch(Exception e)
             {
-                log.LogInformation(e.Message);
+                log.LogError(e.Message);
             } finally
             {
                 sftp.Disconnect();
