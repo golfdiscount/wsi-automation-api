5896769c358c738cad4f8ddbba660f20c4ac7b26
Harmeet Singh <harmeet@golfdiscount.com>
2022-11-22 12:35:31 -0800

    Initial commit

---
+++ Services/SftpService.cs
@@ -0,0 +1,18 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WsiApi.Services
+{
+    public class SftpService
+    {
+        private readonly SftpClient sftp;
+        public SftpService(string host, string user, string pass)
+        {
+            sftp = new(host, user, pass);    
+        }
+    }
+}
