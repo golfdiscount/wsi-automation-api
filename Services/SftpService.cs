4a0257ac5b8eecc3e9b1bd1ebc391624875d9336
Harmeet Singh <harmeet@golfdiscount.com>
2022-11-22 13:08:53 -0800

    Added methods to queue and upload orders

--- Services/SftpService.cs
+++ Services/SftpService.cs
@@ -1,18 +1,72 @@
 ﻿using Renci.SshNet;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WsiApi.Services
 {
     public class SftpService
     {
         private readonly SftpClient sftp;
+        private readonly Queue<Tuple<Stream, string>> sftpQueue;
+
         public SftpService(string host, string user, string pass)
         {
             sftp = new(host, user, pass);    
+        }
+
+        /// <summary>
+        /// Queues a file to be uploaded
+        /// </summary>
+        /// <param name="path">Path to the file on the remote system</param>
+        /// <param name="content">File contents</param>
+        /// <exception cref="ArgumentException">Path or content are either null or have length of 0</exception>
+        public void Queue(string path, Stream content)
+        {
+            if (path == null || path.Length== 0)
+            {
+                throw new ArgumentException("The file path must be longer than 0 characters", nameof(path));
+            }
+
+            if (content == null || content.Length == 0)
+            {
+                throw new ArgumentException("Content cannot be null or have a length of 0", nameof(content));
+            }
+
+            sftpQueue.Enqueue(Tuple.Create(content, path));
+        }
+
+
+        /// <summary>
+        /// Uploads all files in queue
+        /// </summary>
+        /// <returns>Count of files that were successfully uploaded</returns>
+        public int UploadQueue()
+        {
+            Open();
+            int successCount = 0;
+
+            while (sftpQueue.Count > 0)
+            {
+                Tuple<Stream, string> file = sftpQueue.Peek();
+
+                sftp.UploadFile(file.Item1, file.Item2);
+                successCount++;
+            }
+
+            sftp.Disconnect();
+            return successCount;
+        }
+
+        /// <summary>
+        /// Opens an SSH tunnel to the server if it is not already open
+        /// </summary>
+        private void Open()
+        {
+            if (!sftp.IsConnected)
+            {
+                sftp.Connect();
+            }
         }
     }
 }
