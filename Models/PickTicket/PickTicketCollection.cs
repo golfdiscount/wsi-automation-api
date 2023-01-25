6e2b55090857bb483f4b5422c603157b81992ccb
Harmeet Singh <harmeet@golfdiscount.com>
2023-01-24 21:26:50 -0800

    Initial commit

---
+++ Models/PickTicket/PickTicketCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Pgd.Wsi.Models.PickTicket
+{
+    public class PickTicketCollection
+    {
+        public List<PickTicketModel> PickTickets { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int Page { get; set; }
+    }
+}
