75885817861e2254d347ccebb63ccad9174940a7
Ty4jbt <tyler@golfdiscount.com>
2023-11-09 13:20:04 -0800

    created a detail model

---
+++ Models/ShippingConfirmation/ShippingConfirmationDetailModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Pgd.Wsi.Models.ShippingConfirmation
+{
+    public class ShippingConfirmationDetailModel
+    {
+        public int LineNumber { get; set; }
+        public string Sku { get; set; }
+        public int Units { get; set; }
+    }
+}
