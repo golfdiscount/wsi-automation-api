baf35fd341e7fed188d714ca4b229c7dc1fbe24c
Harmeet Singh <harmeet@golfdiscount.com>
2023-11-16 12:56:09 -0800

    Removed unneeded functionality

--- TimerTriggers/ProcessShippingConfirmations.cs
+++ TimerTriggers/ProcessShippingConfirmations.cs
@@ -1,4 +1,3 @@
-using Azure.Storage.Queues;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Renci.SshNet;
@@ -6,9 +5,7 @@ using Renci.SshNet.Sftp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
-using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -16,28 +13,10 @@ namespace Pgd.Wsi.TimerTriggers
 {
     public class ProcessShippingConfirmations
     {
-        private readonly HttpClient shipstation;
-        private readonly QueueServiceClient queueServiceClient;
         private readonly SftpClient wsiSftp;
-        private readonly JsonSerializerOptions jsonOptions;
 
-        private readonly List<string> recipients = new()
+        public ProcessShippingConfirmations(ConnectionInfo sftpConnectionInfo)
         {
-            "harmeet@golfdiscount.com"
-        };
-        private readonly StringBuilder confirmationSummary = new(); // Summary of orders in format: order number, sku, tracking number
-        private readonly Dictionary<string, int> skuCounts = new();
-        private readonly Dictionary<string, HashSet<string>> trackingNumbers = new();
-        private readonly HashSet<string> failedOrders = new();
-
-        public ProcessShippingConfirmations(IHttpClientFactory clientFactory, 
-            JsonSerializerOptions jsonOptions,
-            QueueServiceClient queueServiceClient,
-            ConnectionInfo sftpConnectionInfo)
-        {
-            shipstation = clientFactory.CreateClient("shipstation");
-            this.jsonOptions = jsonOptions;
-            this.queueServiceClient = queueServiceClient;
             this.wsiSftp = new(sftpConnectionInfo);
         }
 
@@ -51,23 +30,6 @@ namespace Pgd.Wsi.TimerTriggers
             foreach (string record in records)
             {
                 ProcessRecord(record);
-            }
-
-            foreach (string orderNumber in trackingNumbers.Keys)
-            {
-                try
-                {
-                    string orderKey = await MarkShipped(orderNumber);
-                    log.LogInformation($"Marked {orderKey} as shipped in ShipStation");
-                } catch (HttpRequestException e)
-                {
-                    log.LogError($"Unable to send request to ShipStation: {e.Message}");
-                    failedOrders.Add(orderNumber);
-                } catch (ArgumentException e)
-                {
-                    log.LogError(e.Message);
-                    failedOrders.Add(orderNumber);
-                }
             }
         }
 
@@ -109,22 +71,6 @@ namespace Pgd.Wsi.TimerTriggers
                 string orderNumber = fields[6];
                 string sku = fields[14];
                 string trackingNumber = fields[32];
-
-                if (!skuCounts.ContainsKey(sku))
-                {
-                    skuCounts[sku] = 0;
-                }
-
-                skuCounts[sku] += int.Parse(fields[35]);
-
-                if (!trackingNumbers.ContainsKey(orderNumber))
-                {
-                    trackingNumbers[orderNumber] = new();
-                }
-
-                trackingNumbers[orderNumber].Add(trackingNumber);
-
-                confirmationSummary.AppendLine($"{orderNumber},{sku},{trackingNumber}");
             }
         }
 
