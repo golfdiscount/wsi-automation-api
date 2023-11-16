e32f9a9aae98ade170b405b342c62c95eb7cad24
Harmeet Singh <harmeet@golfdiscount.com>
2023-11-16 12:55:42 -0800

    Removed SendGrid and queueing functionality

--- TimerTriggers/ProcessShippingConfirmations.cs
+++ TimerTriggers/ProcessShippingConfirmations.cs
@@ -1,10 +1,8 @@
 using Azure.Storage.Queues;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Pgd.Wsi.Models.SendGrid;
 using Renci.SshNet;
 using Renci.SshNet.Sftp;
-using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -71,58 +69,6 @@ namespace Pgd.Wsi.TimerTriggers
                     failedOrders.Add(orderNumber);
                 }
             }
-
-            QueueClient emailQueue = queueServiceClient.GetQueueClient("send-email");
-
-            if (failedOrders.Count > 0)
-            {
-                log.LogError($"There were {failedOrders.Count} orders that were failed to be marked as shipped");
-
-                SendGridMessageModel emailBody = new()
-                {
-                    To = recipients,
-                    Subject = $"{failedOrders.Count} orders failed to be marked as shipped in ShipStation",
-                    Body = $"The following orders could not be marked a shipped in ShipStation: {string.Join(',', failedOrders)}"
-                };
-
-                string emailBodyJson = JsonSerializer.Serialize(emailBody, jsonOptions);
-                emailQueue.SendMessage(emailBodyJson);
-            }
-
-            StringBuilder skuCountCsv = new();
-
-            foreach (string sku in skuCounts.Keys)
-            {
-                skuCountCsv.AppendLine($"{sku}, {skuCounts[sku]}");
-            }
-
-            DateTime today = DateTime.Now;
-            SendGridMessageModel skuCountEmailBody = new()
-            {
-                To = recipients,
-                Subject = $"{skuCounts.Keys.Count} SKU(s) were sent to WSI",
-                Body = $"SKU counts for {today:MM-dd-yyyy}",
-                Attachments = new()
-            };
-
-            Attachment skuCsvAttachment = new()
-            {
-                Filename = $"WSI_SKUs_{today:MM_dd_yyyy}.csv",
-                Content = Convert.ToBase64String(Encoding.UTF8.GetBytes(skuCountCsv.ToString())),
-                Type = "text/csv",
-            };
-
-            Attachment confirmationSummaryAttachment = new()
-            {
-                Filename = $"Shipping Summary {today:MM-dd-yyyy}.csv",
-                Content = Convert.ToBase64String(Encoding.UTF8.GetBytes(confirmationSummary.ToString())),
-                Type = "text/csv"
-            };
-            
-            skuCountEmailBody.Attachments.Add(skuCsvAttachment);
-            skuCountEmailBody.Attachments.Add(confirmationSummaryAttachment);
-            string skuCountEmailBodyJson = JsonSerializer.Serialize(skuCountEmailBody, jsonOptions);
-            emailQueue.SendMessage(skuCountEmailBodyJson);
         }
 
         private string GetShippingConfirmations()
